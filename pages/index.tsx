8794a44eff20daef83af288f118500cac71a9f52
mary <anacoin0014@gmail.com>
2022-12-04 21:39:44 +0330

    create theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
+import { Button } from '@mui/material'
+import Grid  from '@mui/material/Grid'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-     <div>hiii</div>
+     <Grid>hiii
+        <Button variant='outlined'>ddd</Button>
+        <Button variant='contained'>ssss</Button>
+     </Grid>
 
   )
 }
