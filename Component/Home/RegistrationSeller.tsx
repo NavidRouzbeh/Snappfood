8a5a4e0267cbfdde2bc1be5039818339edd66b55
FatemehLavasi <111527593+Fatemehlavasi@users.noreply.github.com>
2022-12-04 22:51:58 -0800

    create component Registration of sellers

---
+++ Component/Home/RegistrationSeller.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Grid from "@mui/material/Grid"
+const RegistrationSeller = () => {
+  return (
+    <Grid>RegistrationSeller</Grid>
+  )
+}
+
+export default RegistrationSeller
