036ca809b71888e6be7e913ba3f42c071aec590a
mobina7849 <mobina.abbasi7849@gmail.com>
2022-12-06 01:13:09 +0330

    create layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,7 @@
-import { Button } from '@mui/material'
 import Grid  from '@mui/material/Grid'
-import Head from 'next/head'
-import Image from 'next/image'
-import Footer from '../Component/Layout/Footer/Footer'
-import FooterDesktop from '../Component/Layout/Footer/FooterDesktop'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-  <Footer/>
+  <Grid>llll</Grid>
 
   )
 }
