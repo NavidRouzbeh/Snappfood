4ab26002fa1784441052529ef585bf8ea0d80743
mary <anacoin0014@gmail.com>
2022-12-05 23:46:08 +0330

    add image change category responsive

--- Component/Home/SectionCategory/CategoryCard.tsx
+++ Component/Home/SectionCategory/CategoryCard.tsx
@@ -1,30 +1,17 @@
 import { Grid, Typography } from "@mui/material"
 import Image from "next/image";
-import pizza from '../../../Assets/Images/HomeImages/CategoryImages/pizza.jpg'
-import berger from '../../../Assets/Images/HomeImages/CategoryImages/berger.jpg'
-
-
 import ArrowBackIosRoundedIcon from '@mui/icons-material/ArrowBackIosRounded';
 
-const categorydata =[
-    {id:1,pic:pizza,title:''},
-    {id:1,pic:pizza,title:''},
-    {id:1,pic:pizza,title:''},
-    {id:1,pic:pizza,title:''},
-    {id:1,pic:pizza,title:''},
-    {id:1,pic:pizza,title:''}
-]
-
-const CategoryCards =()=>{
+const CategoryCards =({pic,title})=>{
     return(
-        <Grid container item xs={4} sm={4} md={1.5} bgcolor={'red'}  alignItems={'center'} flexShrink={0}  height={100} borderRadius={2}>
+        <Grid container item xs={4} sm={4} md={1.5} bgcolor={'white'}  alignItems={'center'} flexShrink={0}  height={100} borderRadius={2}>
             <Grid display={'flex'} justifyContent={'center'} alignItems={'flex-end'} sx={{position:'relative'}}>
                 {/* <Grid> */}
-                    <Image style={{height:90, width:'95%',borderRadius:4,objectFit:'cover'}} src={pizza} alt='' />
+                    <Image style={{height:90, width:'95%',borderRadius:4,objectFit:'cover'}} src={pic} alt='' />
                 {/* </Grid> */}
                 <Grid container sx={{position:'absolute',display:{xs:'none',sm:'flex'}}}>
                     <Grid display={'flex'} alignItems={'center'} bgcolor={'white'} p={'3px'} borderRadius={'4px 0 4px 0'} sx={{cursor:'pointer'}}>
-                        <Typography>dasteh</Typography>
+                        <Typography>{title}</Typography>
                         <ArrowBackIosRoundedIcon fontSize="small"/>
                     </Grid>
                 </Grid>
--- Component/Home/SectionCategory/CategoryContainer.tsx
+++ Component/Home/SectionCategory/CategoryContainer.tsx
@@ -2,6 +2,28 @@ import Grid  from "@mui/material/Grid"
 import Typography from "@mui/material/Typography"
 import CategoryCards from "./CategoryCard"
 
+import pizza from '../../../Assets/Images/HomeImages/CategoryImages/pizza.jpg'
+import berger from '../../../Assets/Images/HomeImages/CategoryImages/berger.jpg'
+import sandwitch from '../../../Assets/Images/HomeImages/CategoryImages/sandwitch.jpg'
+import sokhary from '../../../Assets/Images/HomeImages/CategoryImages/sokhary.jpg'
+import salad from '../../../Assets/Images/HomeImages/CategoryImages/sandwitch.jpg'
+import pasta from '../../../Assets/Images/HomeImages/CategoryImages/pasta.jpg'
+import { StaticImageData } from "next/image"
+import { JsxElement } from "typescript"
+
+const categorydata =[
+    {id:1,pic:pizza,title:'پیتزا'},
+    {id:1,pic:berger,title:'برگر'},
+    {id:1,pic:sandwitch,title:'ساندویچ'},
+    {id:1,pic:sokhary,title:'سوخاری'},
+    {id:1,pic:salad,title:'سالاد'},
+    {id:1,pic:pasta,title:'پاستا'}
+]
+interface CategoryTypes{
+    id:number,
+    pic:StaticImageData,
+    title:string
+}
 const CategoryContainer =()=>{
     return(
         <Grid xs={11} sx={{direction:'rtl'}} >
@@ -9,12 +31,14 @@ const CategoryContainer =()=>{
                 <Typography>دسته بندی</Typography>
             </Grid>
             <Grid container bgcolor={'blue'} xs={12} sx={{flexWrap:{xs:'nowrap',sm:'wrap'},overflowX:{xs:'scroll',sm:'hidden'}}} flexShrink={0} justifyContent={'space-around'} >
+                {categorydata.map((item:CategoryTypes)=>(
+                    <CategoryCards {...item}/>
+                ))}
+                {/* <CategoryCards/>
                 <CategoryCards/>
                 <CategoryCards/>
                 <CategoryCards/>
-                <CategoryCards/>
-                <CategoryCards/>
-                <CategoryCards/>
+                <CategoryCards/> */}
             </Grid>
         </Grid>
     )
