d93b65e057fa7a69ef88352ad70da99114abdcd6
Navid Rouzbeh <Navidrouzbeh@gmail.com>
2022-12-05 21:40:28 +0330

    fix logo svg

--- Component/Layout/Header.tsx
+++ Component/Layout/Header.tsx
@@ -5,17 +5,24 @@ import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import ListAltIcon from '@mui/icons-material/ListAlt';
 import SearchIcon from '@mui/icons-material/Search';
 import logo from "../../Assets/Icons/snappfoodlogo.png";
+import SnappLogo from "../../Assets/Icons/snappfood.svg";
 
-// const theme = useTheme();
-// const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
 
 const Header = () => {
+
+    const theme = useTheme();
+    const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
+
     return(
         
-        <Grid container display={'flex'} justifyContent={'space-between'} alignItems={'center'} sx={{ bgcolor:'#F9FAFB' , width:'100%',height:'4.5rem'}}>
-            <Grid item display={'flex'} alignItems={'center'} >
-                <Image src={logo} style={{width:'68px',height:'34px'}} />
+        <Grid container justifyContent={'space-between'} alignItems={'center'} sx={{ bgcolor:'#F9FAFB' , width:'100%',height:'4.5rem'}}>
+            <Grid item display={'flex'} gap={3} alignItems={'center'} >
+                <Grid>
+                <Image src={SnappLogo} alt="" style={{width:'68px',height:'34px'}} />
+                </Grid>
+                <Grid>
                 <Typography variant="body2">آدرس</Typography>
+                </Grid>
             </Grid>
             <Grid>
             <TextField id="filled-basic" label="جستجو در اسنپ فود" variant="filled" />
