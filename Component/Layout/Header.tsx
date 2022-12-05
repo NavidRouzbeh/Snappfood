94e7c12bf97530441f82fa5e06dfe63636857a01
Navid Rouzbeh <Navidrouzbeh@gmail.com>
2022-12-05 18:27:18 +0330

    Header test

--- Component/Layout/Header.tsx
+++ Component/Layout/Header.tsx
@@ -1,9 +1,31 @@
-import { Grid } from "@mui/material";
+import Image from "next/image";
+import { Grid, Typography, IconButton } from "@mui/material";
+import TextField from '@mui/material/TextField';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import SearchIcon from '@mui/icons-material/Search';
+import logo from "../../Assets/Icons/snappfoodlogo.png";
+
+
 
 const Header = () => {
     return(
-        <Grid>
-            
+        <Grid container display={'flex'} alignItems={'center'} sx={{ bgcolor:'#F9FAFB' , width:'100%',height:'4.5rem'}}>
+            <Grid item display={'flex'} alignItems={'center'} >
+                <Image src={logo} style={{width:'68px',height:'34px'}} />
+                <Typography variant="body2">آدرس</Typography>
+            </Grid>
+            <Grid>
+            <TextField id="filled-basic" label="جستجو در اسنپ فود" variant="filled" />
+            </Grid>
+            <Grid>
+                <IconButton>
+                    <PersonOutlinedIcon/>
+                </IconButton>
+                <IconButton>
+                    <ListAltIcon/>سفارشها
+                </IconButton>
+            </Grid>
         </Grid>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
 import { Button } from '@mui/material'
 import Grid  from '@mui/material/Grid'
-import Head from 'next/head'
-import Image from 'next/image'
+// import Head from 'next/head'
+// import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Header from '../Component/Layout/Header';
 
 export default function Home() {
   return (
-     <Grid>
-
-     </Grid>
-
+    <Header/>
   )
 }
