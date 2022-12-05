9d908895e0a64005f9436d6aa6f7ee8afee17d17
FatemehLavasi <111527593+Fatemehlavasi@users.noreply.github.com>
2022-12-04 23:41:30 -0800

    add component RegistrationSeller.tsx in the index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Grid  from '@mui/material/Grid'
+import RegistrationSeller from '../Component/Home/RegistrationSeller'
 import SectionApp from '../Component/Home/SectionApp'
 export default function Home() {
   return (
-     <Grid container   xs={12} bgcolor={"red"} display={'flex'} justifyContent={"center"} alignItems={"center"}>
+     <Grid container   xs={12} bgcolor={"red"} display={'flex'} flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
       <SectionApp/>
+      <RegistrationSeller/>
      </Grid>
 
   )
