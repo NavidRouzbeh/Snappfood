8f5c2960dfead2f58f83eff1a279006af7b1e02e
FatemehLavasi <111527593+Fatemehlavasi@users.noreply.github.com>
2022-12-05 00:02:39 -0800

    Change bgcolor index & componentRegistrationSeller

--- Component/Home/RegistrationSeller.tsx
+++ Component/Home/RegistrationSeller.tsx
@@ -10,20 +10,20 @@ import FoundationIcon from '@mui/icons-material/Foundation'
 
 const RegistrationSeller = () => {
   return (
-    <Grid container bgcolor={"#EDEFF0"} borderRadius={4} pb={10} sx={{ width:{ xs:"12", lg: "90%"} , display: { sm: "flex", xs: "none" }}} mt={18} display={"flex"} justifyContent={"space-around"}>
+    <Grid container bgcolor={"rgb(249, 250, 251)"} borderRadius={4} pb={10} sx={{ width:{ xs:"12", lg: "90%"} , display: { sm: "flex", xs: "none" }}} mt={18} display={"flex"} justifyContent={"space-around"}>
       <Grid container item position={"relative"} xs={4} ml={10}>
-        <Grid item position={"absolute"} bottom={0.5} right={"3.4375rem"} sx={{display: { md: "flex", xs: "none" } }}  >
+        <Grid item position={"absolute"} bottom={10} right={"3.4375rem"} sx={{display: { md: "flex", xs: "none" } }}  >
           <Image width={424} src={Home} alt={",mnjhbgvf"} />
         </Grid>
       </Grid>
-      <Grid container item md={6}   dir={"rtl"} sx={{paddingRight:{xs:"20px" , md:"0px"} , marginRight:{md:"90px" , xs:"0px"}}} >
-        <Grid item mt={7}>
+      <Grid container item  display={"flex"} md={6}  dir={"rtl"} sx={{ paddingRight:{xs:"20px" , md:"0px"} , marginRight:{md:"90px" , xs:"0px"}}} >
+        <Grid item mt={7} md={12}>
           <Typography variant="h1" >{" صاحب کسب و کار هستید؟"}</Typography>
         </Grid>
-        <Grid item mt={12}>
+        <Grid item mt={12}  md={12}>
           <Typography variant="h3" >{"با اسنپ فود کسب و کارتان را آنلاین کنید و فروشتان را افزایش دهید."} </Typography>
         </Grid>
-        <Grid item mt={8} md={10}  >
+        <Grid item mt={8} md={12}  >
             <Button  sx={{height:"3rem" , fontWeight:"700" , width:"auto" , minWidth:"6.6875rem"}} variant="contained"><FoundationIcon/>{"ثبت نام فروشندگان  "}</Button>
           </Grid>
         </Grid>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import RegistrationSeller from '../Component/Home/RegistrationSeller'
 import SectionApp from '../Component/Home/SectionApp'
 export default function Home() {
   return (
-     <Grid container   xs={12} bgcolor={"red"} display={'flex'} flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
+     <Grid container   xs={12}  display={'flex'} flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
       <SectionApp/>
       <RegistrationSeller/>
      </Grid>
