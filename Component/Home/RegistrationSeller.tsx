020556cfa88f7bc7ac149f66593a8bee2c6df7d5
FatemehLavasi <111527593+Fatemehlavasi@users.noreply.github.com>
2022-12-04 23:40:34 -0800

    add text in component RegistrationSeller.tsx

--- Component/Home/RegistrationSeller.tsx
+++ Component/Home/RegistrationSeller.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Image from 'next/image'
+import Home from "../../Assets/Images/vendor_pic.png"
+
 import Grid from "@mui/material/Grid"
+import Typography from "@mui/material/Typography"
+import Button from "@mui/material/Button"
+
+import FoundationIcon from '@mui/icons-material/Foundation'
+
 const RegistrationSeller = () => {
   return (
-    <Grid>RegistrationSeller</Grid>
+    <Grid container bgcolor={"#EDEFF0"} borderRadius={4} pb={10} sx={{ width:{ xs:"12", lg: "90%"} , display: { sm: "flex", xs: "none" }}} mt={18} display={"flex"} justifyContent={"space-around"}>
+      <Grid container item position={"relative"} xs={4} ml={10}>
+        <Grid item position={"absolute"} bottom={0.5} right={"3.4375rem"} sx={{display: { md: "flex", xs: "none" } }}  >
+          <Image width={424} src={Home} alt={",mnjhbgvf"} />
+        </Grid>
+      </Grid>
+      <Grid container item md={6}   dir={"rtl"} sx={{paddingRight:{xs:"20px" , md:"0px"} , marginRight:{md:"90px" , xs:"0px"}}} >
+        <Grid item mt={7}>
+          <Typography variant="h1" >{" صاحب کسب و کار هستید؟"}</Typography>
+        </Grid>
+        <Grid item mt={12}>
+          <Typography variant="h3" >{"با اسنپ فود کسب و کارتان را آنلاین کنید و فروشتان را افزایش دهید."} </Typography>
+        </Grid>
+        <Grid item mt={8} md={10}  >
+            <Button  sx={{height:"3rem" , fontWeight:"700" , width:"auto" , minWidth:"6.6875rem"}} variant="contained"><FoundationIcon/>{"ثبت نام فروشندگان  "}</Button>
+          </Grid>
+        </Grid>
+      </Grid>   
   )
 }
 
