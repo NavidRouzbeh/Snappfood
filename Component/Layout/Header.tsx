3e997b8d16904d730030b6693cb8b3a173570423
Navid Rouzbeh <Navidrouzbeh@gmail.com>
2022-12-05 19:30:04 +0330

    fix rtl direction

--- Component/Layout/Header.tsx
+++ Component/Layout/Header.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
-import { Grid, Typography, IconButton } from "@mui/material";
+import { Grid, Typography, IconButton, useMediaQuery, useTheme } from "@mui/material";
 import TextField from '@mui/material/TextField';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import ListAltIcon from '@mui/icons-material/ListAlt';
 import SearchIcon from '@mui/icons-material/Search';
 import logo from "../../Assets/Icons/snappfoodlogo.png";
 
-
+// const theme = useTheme();
+// const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
 
 const Header = () => {
     return(
-        <Grid container display={'flex'} alignItems={'center'} sx={{ bgcolor:'#F9FAFB' , width:'100%',height:'4.5rem'}}>
+        
+        <Grid container display={'flex'} justifyContent={'space-between'} alignItems={'center'} sx={{ bgcolor:'#F9FAFB' , width:'100%',height:'4.5rem'}}>
             <Grid item display={'flex'} alignItems={'center'} >
                 <Image src={logo} style={{width:'68px',height:'34px'}} />
                 <Typography variant="body2">آدرس</Typography>
@@ -23,9 +25,10 @@ const Header = () => {
                     <PersonOutlinedIcon/>
                 </IconButton>
                 <IconButton>
-                    <ListAltIcon/>سفارشها
+                    <ListAltIcon/><Typography variant="h3">سفارش ها</Typography>
                 </IconButton>
             </Grid>
+
         </Grid>
     );
 }
