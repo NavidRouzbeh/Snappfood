066afea91d92db8942bc7d83129134f57cc09b53
FatemehLavasi <111527593+Fatemehlavasi@users.noreply.github.com>
2022-12-05 05:12:43 -0800

    add changes component SectionCityes & index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Grid  from '@mui/material/Grid'
 import RegistrationSeller from '../Component/Home/RegistrationSeller'
 import SectionApp from '../Component/Home/SectionApp'
+import SectionCities from '../Component/Home/SectionCities'
 export default function Home() {
   return (
      <Grid container   xs={12}  display={'flex'} flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
       <SectionApp/>
       <RegistrationSeller/>
+      <SectionCities/>
      </Grid>
 
   )
