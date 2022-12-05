db3de5147b483b041413c2da4209ea3f9bccfd6a
FatemehLavasi <111527593+Fatemehlavasi@users.noreply.github.com>
2022-12-04 22:41:42 -0800

    create Sectionapp

---
+++ Component/Home/SectionApp.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+
+import appsection from "../../Assets/Images/app_mockup.png"
+import play from "../../Assets/Images/play.png"
+import Image from 'next/image'
+
+const SectionApp = () => {
+  return (
+
+    <Grid container bgcolor={"#EDEFF0"} borderRadius={"0  0 70px 0"} pb={10} sx={{ width:{ xs:"12", lg: "90%"} , display: { sm: "flex", xs: "none" }}} mt={13} display={"flex"} justifyContent={"space-around"}>
+      njhbg
+    </Grid>
+  )
+}
+
+export default SectionApp
