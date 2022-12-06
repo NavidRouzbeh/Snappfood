99f37f92f91c52690f05e777ed766653460bd0b5
mary <anacoin0014@gmail.com>
2022-12-06 12:52:34 +0330

    style : categorycards

--- Component/Home/SectionCategory/CategoryCard.tsx
+++ Component/Home/SectionCategory/CategoryCard.tsx
@@ -1,26 +1,70 @@
-import { Grid, Typography } from "@mui/material"
+import { Grid, Typography } from "@mui/material";
 import Image from "next/image";
-import ArrowBackIosRoundedIcon from '@mui/icons-material/ArrowBackIosRounded';
+import ArrowBackIosRoundedIcon from "@mui/icons-material/ArrowBackIosRounded";
+import { CategoryTypes } from "./CategoryContainer";
 
-const CategoryCards =({pic,title})=>{
-    return(
-        <Grid container item xs={4} sm={4} md={1.5} bgcolor={'white'}  alignItems={'center'} flexShrink={0}  height={100} borderRadius={2}>
-            <Grid display={'flex'} justifyContent={'center'} alignItems={'flex-end'} sx={{position:'relative'}}>
-                {/* <Grid> */}
-                    <Image style={{height:90, width:'95%',borderRadius:4,objectFit:'cover'}} src={pic} alt='' />
-                {/* </Grid> */}
-                <Grid container sx={{position:'absolute',display:{xs:'none',sm:'flex'}}}>
-                    <Grid display={'flex'} alignItems={'center'} bgcolor={'white'} p={'3px'} borderRadius={'4px 0 4px 0'} sx={{cursor:'pointer'}}>
-                        <Typography>{title}</Typography>
-                        <ArrowBackIosRoundedIcon fontSize="small"/>
-                    </Grid>
-                </Grid>
-            </Grid>
+const CategoryCards = ({ pic, title }: CategoryTypes) => {
+  return (
+    <Grid
+      item
+      xs={4}
+      sm={3.5}
+      md={1.5}
+      alignItems={"center"}
+      flexShrink={0}
+      p={"5px"}
+      height={"fit-content"}
+      borderRadius={2}
+      boxShadow={
+        "rgb(58 61 66 / 6%) 0px 1px 0px, rgb(0 0 0 / 30%) 0px 8px 32px -16px;"
+      }
+    >
+      <Grid display={"flex"} sx={{ position: "relative" }} height={{ md: 100 }}>
+        <Image
+          style={{
+            height: "100%",
+            width: "100%",
+            borderRadius: 4,
+            objectFit: "cover",
+          }}
+          src={pic}
+          alt=""
+        />
+        <Grid
+          container
+          sx={{ position: "absolute", display: { xs: "none", sm: "flex" } }}
+          bottom={0}
+          right={0}
+        >
+          <Grid
+            display={"flex"}
+            alignItems={"center"}
+            bgcolor={"white"}
+            p={"3px"}
+            borderRadius={"4px 0 4px 0"}
+            sx={{ cursor: "pointer" }}
+          >
+            <Typography variant="subtitle2" sx={{ fontWeight: 400 }}>
+              {title}
+            </Typography>
+            <ArrowBackIosRoundedIcon
+              sx={{ fontSize: 15, mr: 2, color: "primary.main" }}
+            />
+          </Grid>
         </Grid>
-    )
-}
+        <Grid
+          display={{ xs: "flex", sm: "none" }}
+          position={"absolute"}
+          right={50}
+          top={30}
+        >
+          <Typography variant="subtitle2" sx={{ color: "white" }}>
+            {title}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default CategoryCards;
-
-// sx={{ display: { xs: "none", md: "contents" } }}
-//maxwidth
--- Component/Home/SectionCategory/CategoryContainer.tsx
+++ Component/Home/SectionCategory/CategoryContainer.tsx
@@ -1,47 +1,54 @@
-import Grid  from "@mui/material/Grid"
-import Typography from "@mui/material/Typography"
-import CategoryCards from "./CategoryCard"
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import CategoryCards from "./CategoryCard";
 
-import pizza from '../../../Assets/Images/HomeImages/CategoryImages/pizza.jpg'
-import berger from '../../../Assets/Images/HomeImages/CategoryImages/berger.jpg'
-import sandwitch from '../../../Assets/Images/HomeImages/CategoryImages/sandwitch.jpg'
-import sokhary from '../../../Assets/Images/HomeImages/CategoryImages/sokhary.jpg'
-import salad from '../../../Assets/Images/HomeImages/CategoryImages/sandwitch.jpg'
-import pasta from '../../../Assets/Images/HomeImages/CategoryImages/pasta.jpg'
-import { StaticImageData } from "next/image"
-import { JsxElement } from "typescript"
+import pizza from "../../../Assets/Images/HomeImages/CategoryImages/pizza.jpg";
+import berger from "../../../Assets/Images/HomeImages/CategoryImages/berger.jpg";
+import sandwitch from "../../../Assets/Images/HomeImages/CategoryImages/sandwitch.jpg";
+import sokhary from "../../../Assets/Images/HomeImages/CategoryImages/sokhary.jpg";
+import salad from "../../../Assets/Images/HomeImages/CategoryImages/salad.jpg";
+import pasta from "../../../Assets/Images/HomeImages/CategoryImages/pasta.jpg";
+import { StaticImageData } from "next/image";
 
-const categorydata =[
-    {id:1,pic:pizza,title:'پیتزا'},
-    {id:1,pic:berger,title:'برگر'},
-    {id:1,pic:sandwitch,title:'ساندویچ'},
-    {id:1,pic:sokhary,title:'سوخاری'},
-    {id:1,pic:salad,title:'سالاد'},
-    {id:1,pic:pasta,title:'پاستا'}
-]
-interface CategoryTypes{
-    id:number,
-    pic:StaticImageData,
-    title:string
+export interface CategoryTypes {
+  id: number;
+  pic: StaticImageData;
+  title: string;
 }
-const CategoryContainer =()=>{
-    return(
-        <Grid xs={11} sx={{direction:'rtl'}} >
-            <Grid xs={12}  p={3}>
-                <Typography>دسته بندی</Typography>
-            </Grid>
-            <Grid container bgcolor={'blue'} xs={12} sx={{flexWrap:{xs:'nowrap',sm:'wrap'},overflowX:{xs:'scroll',sm:'hidden'}}} flexShrink={0} justifyContent={'space-around'} >
-                {categorydata.map((item:CategoryTypes)=>(
-                    <CategoryCards {...item}/>
-                ))}
-                {/* <CategoryCards/>
-                <CategoryCards/>
-                <CategoryCards/>
-                <CategoryCards/>
-                <CategoryCards/> */}
-            </Grid>
-        </Grid>
-    )
-}
+const categorydata: CategoryTypes[] = [
+  { id: 1, pic: pizza, title: "پیتزا" },
+  { id: 2, pic: berger, title: "برگر" },
+  { id: 3, pic: sandwitch, title: "ساندویچ" },
+  { id: 4, pic: sokhary, title: "سوخاری" },
+  { id: 5, pic: salad, title: "سالاد" },
+  { id: 6, pic: pasta, title: "پاستا" },
+];
 
-export default CategoryContainer
+const CategoryContainer = () => {
+  return (
+    <Grid xs={11} sx={{ direction: "rtl" }}>
+      <Grid p={3} mr={{ md: 4 }}>
+        <Typography variant="subtitle2" sx={{ color: "rgb(83, 86, 92)" }}>
+          دسته‌بندی‌ ها
+        </Typography>
+      </Grid>
+      <Grid
+        display={"flex"}
+        height={{ md: 200 }}
+        xs={12}
+        sx={{
+          flexWrap: { xs: "nowrap", sm: "wrap" },
+          overflowX: { xs: "scroll", sm: "hidden" },
+          mr: { md: 8 },
+        }}
+        gap={4}
+      >
+        {categorydata.map((item) => (
+          <CategoryCards {...item} />
+        ))}
+      </Grid>
+    </Grid>
+  );
+};
+
+export default CategoryContainer;
