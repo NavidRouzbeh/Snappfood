bd560849801cc548a9792154a20ac873879ad89d
Navid Rouzbeh <Navidrouzbeh@gmail.com>
2022-12-06 13:54:05 +0330

    feat:add header component in layout

--- Component/Layout/Layout.tsx
+++ Component/Layout/Layout.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from "react";
 import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
 interface LayoutPropsType{
     children:ReactNode
 }
 const Layout = ({children}:LayoutPropsType) => {
     return ( 
         <>
+        <Header/>
         {children}
         <Footer/>
         </>
