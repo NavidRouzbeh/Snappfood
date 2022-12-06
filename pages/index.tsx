0caa57b00f0ec15f7ca934506b5f83324e16dcfc
FatemehLavasi <111527593+Fatemehlavasi@users.noreply.github.com>
2022-12-05 23:12:01 -0800

    add component NavbarSection to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,19 @@
-import Grid  from '@mui/material/Grid'
+import Grid from '@mui/material/Grid'
+import NavbarSection from '../Component/Home/NavbarSection/NavbarSection'
 import RegistrationSeller from '../Component/Home/RegistrationSeller'
 import SectionApp from '../Component/Home/SectionApp'
 import SectionCities from '../Component/Home/SectionCities'
 export default function Home() {
-  return (
+     return (
 
-     <Grid container   xs={12}  display={'flex'} justifyContent={"center"} alignItems={"center"}>
+          <Grid container xs={12} display={'flex'} justifyContent={"center"} flexDirection={"column"} alignItems={"center"}>
+               <NavbarSection />
+               <SectionApp />
+               <RegistrationSeller />
+               <SectionCities />
+          </Grid>
 
-      <SectionApp/>
-      <RegistrationSeller/>
-      <SectionCities/>
-     </Grid>
 
 
-
-  )
+     )
 }
