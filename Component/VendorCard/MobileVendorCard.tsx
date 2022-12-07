d3dd9537fd223a4cb60fb30ec3705b51b3fd9e4e
Navid Rouzbeh <Navidrouzbeh@gmail.com>
2022-12-07 17:13:32 +0330

    document: add mobile vendor title

--- Component/VendorCard/MobileVendorCard.tsx
+++ Component/VendorCard/MobileVendorCard.tsx
@@ -2,11 +2,13 @@ import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import Image from "next/image";
 import Background from "../../Assets/Images/shila.jpg"
+import Logo from "../../Assets/Images/shila_logo.jpg"
+import { Typography } from "@mui/material";
 const MobileVendorCard = () => {
 
     return(
 
-        <Card  sx={{
+        <Card sx={{
             width:'20rem',
             height:'15.5rem',
             borderRadius:'0.4rem'
@@ -19,8 +21,24 @@ const MobileVendorCard = () => {
                 <Image style={{ width:'100%', height:'100%', objectFit:'cover'}} src={Background} alt='' />
             </Grid>
 
-            <Grid>
-                
+            <Grid sx={{position:'absolute', width:'20rem', py:3.5, px:2}}>
+                <Grid sx={{position:'absolute', right:10, top:-50}} >
+                    <Image src={Logo} alt="" style={{ 
+                        width:'4.5rem',
+                        height: '4.5rem',
+                        border: '1px solid rgba(58, 61, 66, 0.06)',
+                        borderRadius: '0.75rem',
+                        }}/>
+                </Grid>
+                <Grid  display={'flex'} justifyContent={'space-between'}>
+                        <Typography variant="h3" sx={{fontSize:'0.8rem'}}>پیتزا شیلا (سعادت آباد)</Typography>
+
+                        <Grid display={'flex'} alignItems={'center'} gap={1}>
+                            <Typography variant="subtitle1" sx={{fontSize:'0.7rem'}}>(۲۳,۳۰۲ )</Typography>
+                            <Typography variant="subtitle1" sx={{fontSize:'0.7rem'}}>۴.۵</Typography>
+                        </Grid>
+
+                </Grid>
             </Grid>
 
         </Card>
