6bf079612381a77ca0480edc4a0d8eb1cd55d213
Navid Rouzbeh <Navidrouzbeh@gmail.com>
2022-12-07 21:37:19 +0330

    doc: create main vendor card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@mui/material'
 import Grid  from '@mui/material/Grid'
-import Card from "../Component/VendorCard/MobileVendorCard"
+import Card from "../Component/VendorCard/VendorCard";
+
 
 export default function Home() {
   return (
