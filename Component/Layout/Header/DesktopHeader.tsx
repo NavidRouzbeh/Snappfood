d7bb6c748fa4f93590109cbacb9de6865eed4177
Navid Rouzbeh <Navidrouzbeh@gmail.com>
2022-12-08 12:05:46 +0330

    fix: search box position

--- Component/Layout/Header/DesktopHeader.tsx
+++ Component/Layout/Header/DesktopHeader.tsx
@@ -28,7 +28,7 @@ const DesktopHeader = () => {
                 <Typography variant="body2"> <Image src={ProfileIcon} alt=""/>تهران، محله سعادت آباد، بلوار جوریکی،نبش کوچه نهم غربی</Typography>
                 </Grid>
             </Grid>
-            <Grid display={{sm:'none', md:"flex"}}>
+            <Grid display={{sm:'none', md:"flex"}} position={'relative'} right={-79} >
                 <Button onClick={handleOpen}
                 style={{ color:'rgb(166, 170, 173)', backgroundColor:'rgb(235, 237, 240)', width: '29vw', height: '3rem', borderRadius: '0.625rem' }}>
                     <SearchIcon/>
