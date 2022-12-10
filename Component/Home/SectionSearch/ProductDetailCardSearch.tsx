a5985a68a879baefab6647a056241bab594593d3
mary <anacoin0014@gmail.com>
2022-12-10 15:02:05 +0330

    style :add image to search

--- Component/Home/SectionSearch/ProductDetailCardSearch.tsx
+++ Component/Home/SectionSearch/ProductDetailCardSearch.tsx
@@ -1,7 +1,14 @@
+import Grid from "@mui/material/Grid/Grid";
+import Image from "next/image";
 
 const ProductDetailCardSearch =()=>{
     return(
-
+        <Grid display={'flex'}>
+            <Grid>
+                {/* <Image /> */}
+            </Grid>
+            <Grid></Grid>
+        </Grid>
     )
 }
 
