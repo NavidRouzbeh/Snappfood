125382b00379e2a6d21c99ad2ccf4802bb494c34
mary <anacoin0014@gmail.com>
2022-12-11 13:08:38 +0330

    feat :add feater to search

--- Component/Home/SectionSearch/SearchBar.tsx
+++ Component/Home/SectionSearch/SearchBar.tsx
@@ -1,6 +1,8 @@
 import Grid from "@mui/material/Grid";
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 // import { useTheme } from "@mui/material";
 import TextField  from "@mui/material/TextField";
 import Divider  from "@mui/material/Divider";
@@ -10,16 +12,46 @@ import {styled} from "@mui/material";
 import NavbarSearch from "./NavbarSearch";
 import HeaderSearch from "./HeaderSearch";
 import ProductDetailCardSearch from "./ProductDetailCardSearch";
+import { useState } from "react";
 
 const SearchBar =()=>{
     // const theme = useTheme()
     // console.log(parseInt('1.25rem'))
+    const [valueSearch,setValueSearch]=useState('')
+    const handleInputSearch=(e:any)=>{
+        setValueSearch(e.target.value)
+    }
     return(
         <Grid display={'flex'} justifyContent={'center'}>
             <Grid position={'relative'} mt={2} width={{xs:'90%',sm:'45vw',md:'29vw'}}>
-                <StyledTextField/>
+                <StyledTextField 
+                    onChange={handleInputSearch}
+                    InputProps={{
+                    startAdornment: (
+                    <InputAdornment position={'start'}>
+                        <SearchOutlinedIcon />
+                    </InputAdornment>
+                    ),
+                }}/>
             </Grid>
-            <Grid sx={{bgcolor: 'background.paper',boxShadow:2,borderRadius:2}} top={{xs:'20%',sm:'12%',md:'11%'}} width={{xs:'95%',sm:'45vw',md:'29vw'}} position={'absolute'} height={"fit-content"}>
+            {(()=>{
+            if(valueSearch===''){
+                return(
+                    <Grid display={{xs:'none',sm:'flex'}} width={{xs:'90%',sm:'45vw',md:'29vw'}} p={2} borderRadius={2} position={'absolute'} bgcolor={'white'} top={{xs:'20%',sm:'12%',md:'11%'}}>
+                        <Typography variant="subtitle1" color={'rgb(83, 86, 92)'}>عبارت مورد نظر خود را وارد کنید</Typography>
+                    </Grid>
+                )
+                
+            }else if(valueSearch.length<=2){
+                return(
+                    <Grid display={{xs:'none',sm:'flex'}} width={{xs:'90%',sm:'45vw',md:'29vw'}} position={'absolute'} bgcolor={'white'} top={{xs:'20%',sm:'12%',md:'11%'}}  p={2} borderRadius={2}>
+                        <Typography ml={0.5} variant="subtitle2" fontWeight={300} fontSize={'0.87rem'}>جستجوی</Typography>
+                        <Typography variant="subtitle2" fontSize={'0.87rem'}>{valueSearch.valueOf()}</Typography>
+                    </Grid>
+                )
+            }else{
+                return(
+                <Grid sx={{bgcolor: 'background.paper',boxShadow:2,borderRadius:2}} top={{xs:'20%',sm:'12%',md:'11%'}} width={{xs:'95%',sm:'45vw',md:'29vw'}} position={'absolute'} height={"fit-content"}>
                     <HeaderSearch/>
                     <Divider/>
                     <Grid display={'flex'} flexDirection={'column'} p={2}>
@@ -39,7 +71,11 @@ const SearchBar =()=>{
                         <NavbarSearch num={70} title={'محصولات'}/>
                         <ProductDetailCardSearch/>
                     </Grid>
-            </Grid>
+                </Grid>
+                )
+            }
+            })()}
+            
         </Grid>
     )
 }
@@ -57,15 +93,11 @@ export const StyledTextField = styled(TextField)(({theme}) => ({
     height: 50,
     borderRadius:8,
     border:'1px solid black',
-    zIndex:12000,
-    ":default":{
-        
-    },
     'text': {
         ...theme.typography['h2'],
         color: 'red',
     },
-
+   
     // 'label.Mui-focused': {
     //     color: 'black',
     //     marginLeft: 0
