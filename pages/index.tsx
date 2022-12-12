efeeb7b62f238ba028e408c71faa256bafe6c131
Navid Rouzbeh <Navidrouzbeh@gmail.com>
2022-12-12 19:38:48 +0330

    fix index

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import Grid from "@mui/material/Grid";
+import NavbarSection from "../Component/Home/NavbarSection/NavbarSection";
+import RegistrationSeller from "../Component/Home/RegistrationSeller";
+import SectionApp from "../Component/Home/SectionApp";
+import CategoryContainer from "../Component/Home/SectionCategory/CategoryContainer";
+import SectionCities from "../Component/Home/SectionCities";
+
+
+import SectionFoodParty from "../Component/Home/SectionFoodParty/SectionFoodParty";
+
+export default function Home() {
+  return (
+    <>
+      <NavbarSection />
+      <Grid
+        container
+        xs={12}
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <CategoryContainer /> 
+        <SectionFoodParty/>
+        <SectionApp />
+        <RegistrationSeller />
+        <SectionCities />
+      </Grid>
+    </>
+  );
+}
