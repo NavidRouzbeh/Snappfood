9b8e02bc1d557f431fc655235946767b61566755
mary <anacoin0014@gmail.com>
2022-12-13 13:02:09 +0330

    style: remove command line

--- Component/Home/SectionCardResturantSwiper/CardResturantContainer.tsx
+++ Component/Home/SectionCardResturantSwiper/CardResturantContainer.tsx
@@ -1,8 +1,12 @@
+import { Typography } from '@mui/material';
 import Grid from '@mui/material/Grid'
 const CardResturantContainer =()=>{
     return(
-        <Grid>
-            
+        <Grid container bgcolor={'red'}>
+            <Grid>
+                <Typography variant='h2'>یک تجربه جدید</Typography>
+                <Typography></Typography>
+            </Grid>
         </Grid>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,9 @@ import Grid from "@mui/material/Grid";
 import NavbarSection from "../Component/Home/NavbarSection/NavbarSection";
 import RegistrationSeller from "../Component/Home/RegistrationSeller";
 import SectionApp from "../Component/Home/SectionApp";
+import CardResturantContainer from "../Component/Home/SectionCardResturantSwiper/CardResturantContainer";
 import CategoryContainer from "../Component/Home/SectionCategory/CategoryContainer";
 import SectionCities from "../Component/Home/SectionCities";
-
-
 import SectionFoodParty from "../Component/Home/SectionFoodParty/SectionFoodParty";
 
 export default function Home() {
