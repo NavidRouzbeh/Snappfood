ae3b5a7f92be216728f864f00f6e843a570aa4ad
FatemehLavasi <111527593+Fatemehlavasi@users.noreply.github.com>
2022-12-13 03:38:54 -0800

    style: changes style  component cart order sidbar

--- Component/Home/SectionOrders/SectionOrdersCart.tsx
+++ Component/Home/SectionOrders/SectionOrdersCart.tsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react'
+
 import Grid from "@mui/material/Grid"
 import Typography from "@mui/material/Typography"
-import CachedIcon from '@mui/icons-material/Cached';
-import Image from 'next/image'
-import image from "../../../Assets/Images/SectionOrders/image.jpeg"
 import SyncProblemIcon from '@mui/icons-material/SyncProblem';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
-import { Button } from '@mui/material';
+import  Button from '@mui/material/Button';
 import SectionOrdersModal from './SectionOrdersModal';
+
+import Image from 'next/image'
+import image from "../../../Assets/Images/SectionOrders/image.jpeg"
+import arrow from "../../../Assets/Svgs/arrow.svg"
+
+
 export interface ModalBoxProps {
     open: boolean,
     setOpen:(arg0: boolean)=> boolean
@@ -29,11 +33,11 @@ const SectionOrdersCart = (): JSX.Element => {
                             alt=""
                         />
                     </Grid>
-                    <Grid item  >
-                        <Grid item ><Typography variant='h5' fontSize={"1rem"} fontWeight={700}>رستوران و کترینک بزرگان </Typography></Grid>
-                        <Grid item display={"flex"} gap={4}>
-                            <Typography  fontSize={"0.75rem"} fontWeight={400}>جمعه 20 آبان </Typography>
-                            <Typography  fontSize={"0.75rem"} fontWeight={400}> {"13:30"}  </Typography>
+                    <Grid item gap={4} >
+                        <Grid item ><Typography mb={0.8} fontSize={"0.8rem"} fontWeight={700}>رستوران و کترینک بزرگان </Typography></Grid>
+                        <Grid item display={"flex"} >
+                            <Typography  color={"#3a3d42"} fontSize={"0.65rem"} fontWeight={400}>جمعه 20 آبان </Typography>
+                            <Typography color={"#3a3d42"}  fontSize={"0.65rem"} fontWeight={400}> {"13:30"}  </Typography>
                         </Grid>
                         <Grid item display={{xs:"flex" , sm:"none"}} >
                             <Typography ml={1.5} fontSize={"12px"} fontWeight={400} color={"#a3a3a3"}> {" تحویل به بلوار بعثت، امام خمینی، خاتم جنوبی،...  "} </Typography>
@@ -44,22 +48,22 @@ const SectionOrdersCart = (): JSX.Element => {
                 </Grid>
                 <Grid item display={{xs:"block" , sm:"none"}} m={1} dir={"ltr"} ><Typography  fontSize={"1rem"} fontWeight={700} >{"138,000 تومان "}</Typography></Grid>
                 <Grid item display={"flex"} flexDirection={"column"} mb={2}>
-                    <Grid display={"flex"} alignItems={"center"} justifyContent={"space-between"} p={0.6} pl={1} pr={1} mt={1} borderRadius={"0.25rem"} sx={{ border: "0.09375rem solid rgba(58, 61, 66, 0.06)" }} bgcolor={"rgba(0, 133, 66, 0.06)"}>
-                        <Typography variant='body2' color={"#3a3d42"} display={{xs:"none" , sm:"block"}}>{"نظرتان را درباره این سفارش به اشتراک بگذارید."}</Typography>
+                    <Grid display={"flex"} alignItems={"center"} justifyContent={"space-between"} p={"10px 17px"} mt={1} borderRadius={"0.25rem"} sx={{ border:{ xs:"0.09375rem solid rgba(58, 61, 66, 0.06)" , sm:"none"} }} bgcolor={"rgba(0, 133, 66, 0.06)"}>
+                        <Typography variant='body2' fontSize={"0.55rem"} color={"#3a3d42"} display={{xs:"none" , sm:"block"}}>{"نظرتان را درباره این سفارش به اشتراک بگذارید."}</Typography>
                         <Typography fontSize={"10px"} fontWeight={500} display={{xs:"block" , sm:"none"}}>{"هنوز به این سفارش امتیازی نداده‌اید"}</Typography>
                         <Grid item display={"flex"} alignItems={'center'}>
-                        <Typography fontWeight={700} variant='subtitle1' color={"rgb(0, 184, 98)"}>{"ثبت نظر "}</Typography>
+                        <Typography fontWeight={900} fontSize={"0.66rem"}  color={"rgb(0, 184, 98)"}>{"ثبت نظر "}</Typography>
                         <Typography  display={{xs:"flex" , sm:"none"}} color={"rgb(0, 184, 98)"}><NavigateBeforeIcon/></Typography>
                         </Grid>
                     </Grid>
                     <Grid display={"flex"} mb={1} flexDirection={{xs:"row-reverse" , sm:"row"}} height={"38px"} justifyContent={"space-between"} alignItems={"center"} mt={1.5}>
-                        <Button onClick={handelOpen} sx={{  bgcolor: { xs: "#FFF", sm: "#EBEDF0" },border:{xs:"solid 1px #404040" , sm:"none"}, borderRadius: "6px",color: { sm: "#000", xs: "#404040" }, gap:0.6, width: "48%", '&:hover': { backgroundColor: {xs:"#FFF" , sm:"#EBEDF0"} }}} >
-                            <Typography display={{xs:"none" , sm:"flex"}}><SyncProblemIcon /></Typography>
-                            <Typography fontSize={"0.875rem"}fontWeight={700} >مشاهده فاکتور </Typography>
+                        <Button onClick={handelOpen} sx={{  bgcolor: { xs: "#FFF", sm: "#3a3d420f" },border:{xs:"solid 1px #404040" , sm:"none"}, borderRadius: "6px",color: { sm: "#3a3d42", xs: "#404040" }, gap:1, width: "48%", '&:hover': { backgroundColor: {xs:"#FFF" , sm:"#EBEDF0"} }}} >
+                            <Typography display={{xs:"none" , sm:"flex"}}><SyncProblemIcon style={{fontSize:"19px" }}/></Typography>
+                            <Typography fontSize={"0.8rem"}fontWeight={900} >مشاهده فاکتور </Typography>
                         </Button>
-                        <Button  sx={{ bgcolor: { xs: "#ff00a4", sm: "#EBEDF0" } , borderRadius: "6px", color: { sm: "#000", xs: "#FFF" }, gap:0.6, width: "48%", '&:hover': { backgroundColor:{xs:"#ff00a4" , sm:"#EBEDF0"}}  }}>
-                           <Typography display={{xs:"none" , sm:"flex"}} ><CachedIcon /></Typography>
-                            <Typography fontSize={"0.875rem"}fontWeight={700} >سفارش مجدد </Typography>  
+                        <Button  sx={{ bgcolor: { xs: "#ff00a4", sm: "#3a3d420f" } , borderRadius: "6px", color: { sm: "#3a3d42", xs: "#FFF" }, gap:1, width: "48%", '&:hover': { backgroundColor:{xs:"#ff00a4" , sm:"#EBEDF0"}}  }}>
+                           <Typography display={{xs:"none" , sm:"flex"}} ><Image src={arrow} alt={"jhbbbhjnkml"}/></Typography>
+                            <Typography fontSize={"0.8rem"}fontWeight={900} >سفارش مجدد </Typography>  
                         </Button>
                     </Grid>
                 </Grid>
