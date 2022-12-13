e4f98ec7a6ee7b2bf0bb4d1489a0a817d7f04233
mary <anacoin0014@gmail.com>
2022-12-13 11:01:56 +0330

    style:create resturant card container

---
+++ Component/Home/SectionCardResturantSwiper/CardResturantContainer.tsx
@@ -0,0 +1,10 @@
+import Grid from '@mui/material/Grid'
+const CardResturantContainer =()=>{
+    return(
+        <Grid>
+            
+        </Grid>
+    )
+}
+
+export default CardResturantContainer;
