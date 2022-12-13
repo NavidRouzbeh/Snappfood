c4ec8ff58e50f4d3ee37412a9f541c8d12e42bf8
mary <anacoin0014@gmail.com>
2022-12-13 09:52:28 +0330

    fix :delete command in styled textfeild

--- Styled/StyledTextField.ts
+++ Styled/StyledTextField.ts
@@ -4,13 +4,7 @@ import TextField  from "@mui/material/TextField";
 export const StyledTextField = styled(TextField)(({theme}) => ({
     width: '100%',
     background: 'white',
-    // height: 55,
     borderRadius:8,
-    // color:'red',
-    // 'text': {
-    //     ...theme.typography['h2'],
-    //     color: 'red',
-    // }, 
     // 'label.Mui-focused': {
     //     color: 'black',
     //     marginLeft: 0
@@ -27,19 +21,6 @@ export const StyledTextField = styled(TextField)(({theme}) => ({
         '&.Mui-focused':{
             background:'#F9FAFB',
             borderRadius: '8px',
-            // border:'1px solid red',
-            // '&$focused $notchedOutline': {
-            //     borderColor: '#2EFF22',
-            //     borderWidth: 1,
-            // },
         },
-        // '&.Mui-focused fieldset': {
-        //     height:48,
-        //     background:'#F9FAFB',
-        //     zIndex:0
-        // },
     },
-    // 'fieldset': {
-    //     border: 1
-    // },
 }))
