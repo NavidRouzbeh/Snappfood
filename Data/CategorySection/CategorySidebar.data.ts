2a5dc97dbeb82378a9560a115a98fbeb8cbcee13
Navid Rouzbeh <Navidrouzbeh@gmail.com>
2022-12-16 16:41:55 +0330

    fix: fix sidebar images

--- Data/CategorySection/CategorySidebar.data.ts
+++ Data/CategorySection/CategorySidebar.data.ts
@@ -1,10 +1,10 @@
 import { StaticImageData } from "next/image"
-import pizza from "../../Assets/Images/CategoryPageSidebar/pizza.png";
-import persian from "../../Assets/Images/CategoryPageSidebar/aash.png";
-import kabab from "../../Assets/Images/CategoryPageSidebar/kabab.png";
-import salad from "../../Assets/Images/CategoryPageSidebar/salad.png";
-import seafood from "../../Assets/Images/CategoryPageSidebar/daryayi.png";
-import international from "../../Assets/Images/CategoryPageSidebar/sooshi.png";
+import pizza from "../../Assets/Images/HomeImages/CategoryImages/pizza.jpg";
+import burger from "../../Assets/Images/HomeImages/CategoryImages/berger.jpg";
+import sandwitch from "../../Assets/Images/HomeImages/CategoryImages/sandwitch.jpg";
+import fried from "../../Assets/Images/HomeImages/CategoryImages/sokhary.jpg";
+import salad from "../../Assets/Images/HomeImages/CategoryImages/salad.jpg";
+import pasta from "../../Assets/Images/HomeImages/CategoryImages/pasta.jpg";
 
 export interface CategorySidebarType{
     id:number
@@ -14,10 +14,10 @@ export interface CategorySidebarType{
 
 
 export const CategorySidebarData:CategorySidebarType[]=[
-    {id:1 , image:pizza , title:"فست‌فود"},
-    {id:2 , image:persian , title:"ایرانی "},
-    {id:3 , image:kabab , title:"کباب"},
-    {id:4 , image:salad , title:"سالاد"},
-    {id:5 , image:seafood , title:"دریایی"},
-    {id:6 , image:international , title:"بین‌الملل"}
+    {id:1 , image:pizza , title:"پیتزا"},
+    {id:2 , image:burger , title:"برگر"},
+    {id:3 , image:sandwitch , title:"ساندویچ"},
+    {id:4 , image:fried , title:"سوخاری"},
+    {id:5 , image:pasta , title:"پاستا"},
+    {id:6 , image:salad , title:"سالاد"}
 ]
