3bbe586e0f4433dd6b88534445e4b023c23a11e6
mary <anacoin0014@gmail.com>
2022-12-16 18:55:28 +0330

    feat :create database

---
+++ Data/Main/MainData.tsx
@@ -0,0 +1,29 @@
+
+export const DataBase = [
+  { id: 1, 
+    name: "شیلا", 
+    star:4,
+    score:6056,
+    address:'',
+    delivery:5000,
+    logo:'',
+    picture:'',
+    coupon:false,
+    discount:true,
+    categories: ["پیتزا", "سوخاری",'نوشیدنی','پیش غدا','خاچاپوری','هات داگ'] ,
+    foods:[
+        {id:1,category:'پیتزا',name:'',images:'',price:145000},
+        {id:2,category:'پیتزا',name:'',images:'',price:165000},
+        {id:3,category:'پیتزا',name:'',images:'',price:145000},
+        {id:4,category:'سوخاری',name:'',images:'',price:145000},
+        {id:5,category:'سوخاری',name:'',images:'',price:145000},
+        {id:6,category:'سوخاری',name:'',images:'',price:145000},
+        {id:7,category:'نوشیدنی',name:'',images:'',price:145000},
+        {id:8,category:'نوشیدنی',name:'',images:'',price:145000},
+        {id:9,category:'نوشیدنی',name:'',images:'',price:145000},
+        {id:10,category:'پیش غذا',name:'',images:'',price:145000},
+        {id:11,category:'پیش غذا',name:'',images:'',price:145000},
+        {id:12,category:'پیش غذا',name:'',images:'',price:145000},
+    ]
+    },
+];
