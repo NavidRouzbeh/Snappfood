1c4e361a31758e6d988a8207800bd035f9e11312
Navid Rouzbeh <Navidrouzbeh@gmail.com>
2022-12-27 17:17:52 +0330

    1

---
+++ pages/category/[categoryName].tsx
@@ -0,0 +1,47 @@
+import { PanoramaSharp } from "@mui/icons-material";
+import { Grid } from "@mui/material";
+import { GetStaticProps, GetStaticPropsContext } from "next";
+import { getMarkets } from "../../API/getMarkets";
+import CategorySidebarContainer from "../../Component/CategorySidebar/CategorySidebarContainer";
+import SortingDropDown from "../../Component/SortingDropDown/SortingDropDown";
+import VendorCard from "../../Component/VendorCard/VendorCard";
+import { categoryData } from "../../Data/Main/categoryData";
+interface CategoryNameProps {
+    markets:[]
+}
+
+
+const CategoryName = ({markets}:CategoryNameProps) => {
+    console.log(markets)
+    return(
+
+        <Grid>
+            <SortingDropDown/>
+            <VendorCard/>
+            <CategorySidebarContainer/>
+        </Grid>
+    )
+
+}
+
+export async function getStaticProps(contex:GetStaticPropsContext) {
+
+    const markets = await getMarkets(contex.params?.categoryName)  
+
+    return {
+      props: {
+        markets,
+      },
+    }
+  }
+  
+
+  export async function getStaticPaths() {
+    return {
+      paths: categoryData.map(item => ({params:{categoryName:item}})) ,
+      fallback: true,
+    }
+  }
+  
+export default CategoryName;
+
