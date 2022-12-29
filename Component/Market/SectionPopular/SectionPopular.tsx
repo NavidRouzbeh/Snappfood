d98057883ac26b0b3e2905f0d56b1b108661ae3e
mobina7849 <mobina.abbasi7849@gmail.com>
2022-12-29 19:57:38 +0330

    refactor:delete extra code

--- Component/Market/SectionPopular/SectionPopular.tsx
+++ Component/Market/SectionPopular/SectionPopular.tsx
@@ -1,21 +1,9 @@
 import { Grid, Typography } from "@mui/material";
-import { wrap } from "module";
-import { useRef } from "react";
 import Cardpopular from "./CardPopular";
 interface SectionPopularType {
   market:{}
 }
 const SectionPopular=({market}:SectionPopularType)=>{
-  // const foodRef=useRef<null>(null)
-  // const apptizerRef=useRef<null>(null)
-  // const drinkRef=useRef<null>(null)
-  // const scrollToSection=(elementRef:any)=>{
-  //   window.scrollTo({
-  //     top:elementRef.current.offsetTop,
-  //     behavior:"smooth"
-  //   })
-  // }
-  console.log(market[0])
     return(
       
 
@@ -36,29 +24,6 @@ const SectionPopular=({market}:SectionPopularType)=>{
         ))}
         </Grid>
            ))}
-
-        {/* <Grid xs={12} textAlign={'center'} p={2} >{' غذاها'}</Grid>
-        <Grid display={'flex'} flexWrap={'wrap'} >
-          <Cardpopular/>
-          <Cardpopular/>
-          <Cardpopular/>
-          <Cardpopular/>
-        </Grid>
-        <Grid xs={12} textAlign={'center'} p={2} ref={apptizerRef}>{' پیش غذا'}</Grid>
-        <Grid display={'flex'} flexWrap={'wrap'} >
-          <Cardpopular/>
-          <Cardpopular/>
-          <Cardpopular/>
-          <Cardpopular/>
-        </Grid>
-       <Grid xs={12} textAlign={'center'} p={2} ref={drinkRef}>{' نوشیدنی'}</Grid>
-        <Grid display={'flex'} flexWrap={'wrap'} >
-          <Cardpopular/>
-          <Cardpopular/>
-          <Cardpopular/>
-          <Cardpopular/>
-        </Grid>
-      </Grid> */}
     </Grid>
   </Grid>
 
