6e7922d562f0c3cb73dbf45c88509961bc2719c7
Marykn78 <anacoin0014@gmail.com>
2022-12-29 23:35:50 -0800

    fix :Add data to maindata

--- Component/Market/SectionPopular/CardPopular.tsx
+++ Component/Market/SectionPopular/CardPopular.tsx
@@ -14,7 +14,7 @@ const Cardpopular=({food}:CardpopularProps)=>{
               <Typography variant="subtitle1">{food.material}</Typography>
              </Grid>
              <Grid >
-               <Image src={food.image} alt={'food'} width={112} height={112} style={{borderRadius:4}}/>
+               <Image src={food.images} alt={'food'} width={112} height={112} style={{borderRadius:4}}/>
              </Grid>
            </Grid>
            <Grid display={'flex'} justifyContent={'space-between'}>
